032999af5600c5cfea7adab9d946d4b60e95a0a1
Amaro-Koberle <amaro@amarokoberle.com>
2022-03-21 20:21:28 +0100

    home route now redirects to /nodes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,13 @@
+import { redirect } from 'remix';
+
+export const loader = () => {
+	return redirect('/nodes');
+};
+
 export default function IndexRoute() {
-	return <div></div>;
+	return (
+		<>
+			<p>Index Route</p>
+		</>
+	);
 }
