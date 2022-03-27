6350101d85f894c792aa1d2ad72ecaaca95f8d39
Amaro-Koberle <amaro@amarokoberle.com>
2022-03-27 20:30:07 +0200

    creating incoming links now works

--- app/routes/links/new.$sourceNodeId.$targetNodeId.tsx
+++ app/routes/links/new.$sourceNodeId.$targetNodeId.tsx
@@ -2,13 +2,16 @@ import type { ActionFunction, LoaderFunction } from 'remix';
 import {
 	useActionData,
 	useLoaderData,
-	useParams,
 	json,
+	Form,
 	Link,
 	redirect,
 } from 'remix';
-import type { Node } from '@prisma/client';
 
+import { MdOutlineEast } from 'react-icons/md';
+import { MdArrowBackIos } from 'react-icons/md';
+
+import type { Node } from '@prisma/client';
 import { db } from '~/utils/db.server';
 
 function validateNodeName(name: string) {
@@ -81,7 +84,6 @@ export const loader: LoaderFunction = async ({ params }) => {
 };
 
 export default function NewLinkRoute() {
-	const params = useParams();
 	const actionData = useActionData<ActionData>();
 	const data = useLoaderData<LoaderData>();
 
@@ -89,61 +91,94 @@ export default function NewLinkRoute() {
 		<div>
 			{!data.targetNode && data.sourceNode ? (
 				<div>
-					<p>{`Create a link from '${data.sourceNode.name}'.`}</p>
+					<p className='text-center text-sm text-stone-600'>{`Select a target for a link leaving from '${data.sourceNode.name}'.`}</p>
 					<ul>
 						{data.nodeListItems.map((node) => (
 							<li key={node.id}>
-								<Link to={`/links/new/${params.sourceNodeId}/${node.id}`}>
-									{node.name}
+								<Link to={`/links/new/${data.sourceNode.id}/${node.id}`}>
+									<p className='my-3'>{node.name}</p>
+									<hr className='border-b border-stone-300' />
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			) : data.targetNode && !data.sourceNode ? (
+				<div>
+					<p className='text-center text-sm text-stone-600'>{`Select a source for a link pointing to '${data.targetNode.name}'.`}</p>
+					<ul>
+						{data.nodeListItems.map((node) => (
+							<li key={node.id}>
+								<Link to={`/links/new/${node.id}/${data.targetNode.id}`}>
+									<p className='my-3'>{node.name}</p>
+									<hr className='border-b border-stone-300' />
 								</Link>
 							</li>
 						))}
 					</ul>
 				</div>
 			) : (
-				<form method='post'>
-					<div>
-						<label>
-							Link name
+				<div>
+					<header className='flex items-center justify-between text-xl mx-2'>
+						<Link to='/nodes/' className=''>
+							<MdArrowBackIos />
+						</Link>
+						<h2>Create link</h2>
+						<div className='w-4'></div>
+					</header>
+					<Form className='mt-4' method='post'>
+						<div>
+							<label className='rounded-xl border border-stone-900 flex flex-col p-2'>
+								<span className='text-sm text-stone-600'>Link name</span>
+								<input
+									type='text'
+									className='bg-stone-200'
+									name='name'
+									defaultValue={actionData?.fields?.name}
+									aria-invalid={
+										Boolean(actionData?.fieldErrors?.name) || undefined
+									}
+									aria-errormessage={
+										actionData?.fieldErrors?.name ? 'name-error' : undefined
+									}
+								/>
+							</label>
+							{actionData?.fieldErrors?.name ? (
+								<p role='alert' id='name-error'>
+									{actionData.fieldErrors.name}
+								</p>
+							) : null}
+						</div>
+						<div>
 							<input
-								type='text'
-								name='name'
-								defaultValue={actionData?.fields?.name}
-								aria-invalid={
-									Boolean(actionData?.fieldErrors?.name) || undefined
-								}
-								aria-errormessage={
-									actionData?.fieldErrors?.name ? 'name-error' : undefined
-								}
+								name='sourceNodeId'
+								value={data.sourceNode?.id}
+								type='hidden'
 							/>
-						</label>
-						{actionData?.fieldErrors?.name ? (
-							<p className='form-validation-error' role='alert' id='name-error'>
-								{actionData.fieldErrors.name}
-							</p>
-						) : null}
-					</div>
-					<div>
-						<input
-							name='sourceNodeId'
-							value={data.sourceNode?.id}
-							type='hidden'
-						/>
-						<input
-							name='targetNodeId'
-							value={data.targetNode?.id}
-							type='hidden'
-						/>
-					</div>
-					<div>
-						{actionData?.formError ? (
-							<p className='form-validation-error' role='alert'>
-								{actionData.formError}
-							</p>
-						) : null}
-						<button type='submit'>Add</button>
-					</div>
-				</form>
+							<input
+								name='targetNodeId'
+								value={data.targetNode?.id}
+								type='hidden'
+							/>
+						</div>
+						<div>
+							{actionData?.formError ? (
+								<p role='alert'>{actionData.formError}</p>
+							) : null}
+							<button
+								type='submit'
+								className='rounded-xl bg-stone-800 text-stone-200 py-2 px-4 gap-4 items-center flex flex-nowrap w-full my-2'>
+								<MdOutlineEast className='text-2xl' />
+								<div className='flex flex-col text-left'>
+									<p>Create link</p>
+									<p className='text-sm text-stone-400'>
+										{`Link from ${data.sourceNode.name} to ${data.targetNode.name}`}
+									</p>
+								</div>
+							</button>
+						</div>
+					</Form>
+				</div>
 			)}
 		</div>
 	);
